0955dac6e4623437ec6ce683e3ac88ccc1e58f19
Pablo Abratte <pabratte@gmail.com>
2019-01-29 16:03:17 -0300

    Desafío: pintar_escena

---
+++ src/desafios/pintar_escena/README.md
@@ -0,0 +1,9 @@
+# Desafío #1: Pintar la escena
+
+Utilizando las imágenes que acompañan al ejemplo, agregar los sprites necesarios para armar (más o menos) la escena de la imagen
+
+![](finished_scene.png)
+
+A modo de ayuda, el ejemplo contiene algunos sprites ya agregados.
+
+También puede utilizarse la imagen ![](help.png) a modo de guía para ubicar los sprites. El tamaño de cada celda de la grilla es de 21 px. 
--- src/desafios/pintar_escena/source/PlayState.hx
+++ src/desafios/pintar_escena/source/PlayState.hx
@@ -5,41 +5,24 @@ import flixel.FlxSprite;
 
 class PlayState extends FlxState
 {
-	var background: FlxSprite;
-	var door: 		FlxSprite;
-	var bee: 		FlxSprite;
-	var key: 		FlxSprite;
 	var block1: 	FlxSprite;
-	var block2: 	FlxSprite;
-	var itemBlock: 	FlxSprite;
 	var coin1: 		FlxSprite;
-	var coin2: 		FlxSprite;
-	var char1: 		FlxSprite;
-	var char2: 		FlxSprite;
 
 	override public function create():Void
 	{
 		super.create();
-		background = new FlxSprite(0, 0, AssetPaths.background__png);
+	
+		coin1 	= new FlxSprite(2*BLOCK_SIZE, 6*BLOCK_SIZE, AssetPaths.coin__png);
+		block1 	= new FlxSprite(8*BLOCK_SIZE, 4*BLOCK_SIZE, AssetPaths.block_brick__png);
 
-		coin1 	= new FlxSprite(2*21, 6*21, AssetPaths.coin__png);
-		coin2 	= new FlxSprite(3*21, 5*21, AssetPaths.coin__png);
-		door 	= new FlxSprite(3*21, 5*21, AssetPaths.door__png);
-		key 	= new FlxSprite();
-		block1 	= new FlxSprite();
-		block2 	= new FlxSprite();
-		char1	= new FlxSprite(7*21, 5*21, AssetPaths.char1__png);
-		char2	= new FlxSprite(12*21, 5*21, AssetPaths.char1__png);
-		char2.flipX = true;
-		add(background);
 		add(coin1);
-		add(coin2);
-		add(char1);
-		add(char2);
+		add(block1);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
 	}
+
+	static inline var BLOCK_SIZE: Int = 21;
 }
