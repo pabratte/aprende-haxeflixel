559c702df083a9469f834ecc1f42dcb46873f103
Pablo Abratte <pabratte@gmail.com>
2019-09-23 00:05:38 -0300

    Actualizado tilemap_tiled

--- src/ejemplos/tilemap_tiled/source/PlayState.hx
+++ src/ejemplos/tilemap_tiled/source/PlayState.hx
@@ -1,84 +1,93 @@
-import flixel.FlxG;
+package;
+
 import flixel.FlxState;
 import flixel.FlxObject;
-import flixel.group.FlxGroup;
 import flixel.tile.FlxBaseTilemap;
 import flixel.tile.FlxTilemap;
+import flixel.FlxG;
 import flixel.addons.editors.tiled.TiledMap;
 import flixel.addons.editors.tiled.TiledTileLayer;
+import flixel.addons.editors.tiled.TiledObjectLayer;
+import flixel.addons.editors.tiled.TiledObject;
+import flixel.group.FlxGroup;
+import flixel.FlxCamera;
 
 class PlayState extends FlxState
 {
 	override public function create():Void
 	{
 		super.create();
-		bgColor = 0xff849684;
-		level = new TiledLevel("assets/data/levels/level01.tmx");
+		hero = new Hero(128, 64);
+		var tiledMap: TiledMap;
+		tiledMap = new TiledMap("assets/data/map.tmx");
 		
-		// create groups
-		enemies = new FlxTypedGroup<Enemy>();
-		coins = new FlxTypedGroup<Coin>();
+		var solidLayer = cast(tiledMap.getLayer("solid"), TiledTileLayer);
+		
+		map = new FlxTilemap();
+		map.loadMapFromArray(solidLayer.tileArray, tiledMap.width, tiledMap.height,"assets/images/tiles_alpha.png", tiledMap.tileWidth, tiledMap.tileHeight, FlxTilemapAutoTiling.OFF, 1);
+		bgColor = 0xff363636;
+
+		FlxG.camera.follow(hero, TOPDOWN);
+		FlxG.camera.setScrollBoundsRect(0, 0, tiledMap.fullWidth, tiledMap.fullHeight, true);
+
+		coins = new FlxGroup();
+		enemies = new FlxGroup();
 		heroCollideables = new FlxGroup();
-		levelCollideables = new FlxGroup();
-		
-		// load objects
-		level.loadObjects(loadObjectCallback);
-		
-		levelCollideables.add(hero);
-		levelCollideables.add(enemies);
+
+		var objectLayer = cast(tiledMap.getLayer("entities"), TiledObjectLayer);
+		for(i in 0...objectLayer.objects.length){
+			loadObject(objectLayer.objects[i]);
+		}
+
+		heroCollideables.add(map);
 		heroCollideables.add(enemies);
 		heroCollideables.add(coins);
 
-		add(level.foregroundTiles);
+		add(map);
 		add(coins);
 		add(enemies);
 		add(hero);
 	}
 
-	function loadObjectCallback(type: String, x: Int, y: Int){
-		switch (type){
-			case "player_start":
-				hero = new Hero(x, y);
-			case "enemy":
-				var enemy = new Enemy(x, y);
-				enemies.add(enemy);
-			case "coin":
-				coins.add(new Coin(x, y));
-			default:
-				trace("Unknow object type: "+type);
-		}
-	}
-
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
-		level.collideWithLevel(levelCollideables);
-		
-		FlxG.overlap(heroCollideables, hero, onCollisionWithChar);
-		//FlxG.collide(level.foregroundTiles, enemies);
-		if(FlxG.keys.justPressed.R)
-		{
-			FlxG.resetState();
+		FlxG.collide(map, enemies);
+		FlxG.overlap(heroCollideables, hero, onHeroCollision);
+	}
+
+	function loadObject(obj: TiledObject){
+		var type = obj.type;
+		if(type == "Coin"){
+			var points = obj.properties.get("points");
+			var coin = new Coin(obj.x, obj.y-16);
+			coins.add(coin);
+		}else if(type == "Enemy"){
+			var enemy = new Enemy(obj.x, obj.y-16);
+			enemies.add(enemy);
+		}else if(type == "Player"){
+			hero.x = obj.x;
+			hero.y = obj.y-16;
+		}else{
+			trace("ERROR: no reconozco el tipo "+ obj.type);
 		}
 	}
 
-	private function onCollisionWithChar(theObject: FlxObject, hero: Hero)
-	{
-		if(Type.getClass(theObject) == Enemy)
-		{
+	function onHeroCollision(object: FlxObject, hero: Hero){
+		if(Type.getClass(object) == Enemy){
 			hero.hit();
-		}else if(Type.getClass(theObject) == Coin){
-			var theCoin = cast(theObject, Coin);
-			theCoin.pick();
+		}else if(Type.getClass(object) == Coin){
+			var coin = cast(object, Coin);
+			coin.pick();
+		}else if(Type.getClass(object) == FlxTilemap){
+			FlxObject.separate(object, hero);
 		}
 	}
 
-	private var hero: Hero;
-	private var level: TiledLevel;
-	private var enemies: FlxTypedGroup<Enemy>;
-	private var coins: FlxTypedGroup<Coin>;
-	private var heroCollideables: FlxGroup;			// things that can collide with the Hero
-	private var levelCollideables: FlxGroup;		// things that can collide with the level
-	
 
+	var map: FlxTilemap;
+	var hero: Hero;
+	var coins: FlxGroup;
+	var enemies: FlxGroup;
+	var heroCollideables: FlxGroup;
 }
